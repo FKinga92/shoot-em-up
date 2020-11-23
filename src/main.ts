b5f718641a4b696474d86c252105aaad20071005
FKinga92 <felfalusi.kinga@gmail.com>
2020-11-23 21:58:35 +0100

    fix spritesLoaded method

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Loader, Sprite } from 'pixi.js';
+import { Application, Sprite } from 'pixi.js';
 import Scroller from './background/scroller';
 
 export default class Main {
@@ -22,11 +22,11 @@ export default class Main {
       .add('ship', 'assets/ship.png')
       .add('bg', 'assets/bg.gif')
       .add('starfield', 'assets/starfield.png')
-      .load((loader) => this._spritesLoaded(loader));
+      .load(this._spritesLoaded.bind(this));
   }
 
-  private _spritesLoaded(loader: Loader) {
-    const ship = new Sprite(loader.resources.ship.texture);
+  private _spritesLoaded() {
+    const ship = new Sprite(this._app.loader.resources.ship.texture);
     this._scroller = new Scroller(this._app.stage);
 
     ship.x = 10;
@@ -34,6 +34,6 @@ export default class Main {
     ship.scale.set(1.5, 1.5);
     this._app.stage.addChild(ship);
 
-    this._app.ticker.add(() => this._update);
+    this._app.ticker.add(this._update.bind(this));
   }
 }
