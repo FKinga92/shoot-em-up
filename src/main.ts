8a9dcb7c1a561057182df62843f7d24aa15babba
FKinga92 <felfalusi.kinga@gmail.com>
2020-11-24 11:20:06 +0100

    animate ship + user can shoot a missile by pressing space

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ export default class Main {
   private _scroller: Scroller;
   private _ship: Ship;
 
+  private bullets: Array<any>;
+  private bulletSpeed: number;
+
   constructor() {
     this._app = new Application({
       width: CANVAS_WIDTH,
@@ -21,23 +24,35 @@ export default class Main {
 
   private _update() {
     this._scroller.moveViewPortXBy(1); // TODO
+
+    // TODO move this to another class
+    for (let b = this.bullets.length - 1; b >= 0; b--) {
+      this.bullets[b].position.x += this.bulletSpeed;
+    }
   }
 
   private _loadSprites() {
+    const shipFrames = ['assets/ship/f1.png', 'assets/ship/f2.png', 'assets/ship/f3.png', 'assets/ship/f4.png'];
     this._app.loader
-      .add('ship', 'assets/ship.png')
-      .add('bg', 'assets/bg.gif')
-      .add('starfield', 'assets/starfield.png')
+      .add(shipFrames)
+      .add('assets/bg.gif')
+      .add('assets/starfield.png')
+      .add('assets/missile.png')
       .load(this._spritesLoaded.bind(this));
   }
 
   private _spritesLoaded() {
     this._scroller = new Scroller(this._app.stage);
     this._ship = new Ship(this._app);
+    this.bullets = [];
+    this.bulletSpeed = 5;
 
     document.addEventListener('keydown', (e) => {
       if (e.code.startsWith('Arrow')) {
         this._ship.move(e.code.replace('Arrow', '').toLowerCase());
+      }
+      if (e.code === 'Space') {
+        this._ship.shoot(this.bullets);
       }
     });
 
--- src/ship.ts
+++ src/ship.ts
@@ -1,18 +1,24 @@
-import { Application, Sprite, Texture } from 'pixi.js';
+import { AnimatedSprite, Application, Container, Sprite, Texture } from 'pixi.js';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from './constants';
 
-type Direction = 'up' | 'down' | 'left' | 'right';
+export default class Ship extends AnimatedSprite {
 
-export default class Ship extends Sprite {
+  private static SPEED = 10;
+
+  private stage: Container;
 
   constructor(app: Application) {
-    const texture = Texture.from('assets/ship.png');
-    super(texture);
+    const frames = ['assets/ship/f1.png', 'assets/ship/f2.png', 'assets/ship/f3.png', 'assets/ship/f4.png'];
+    const textures = frames.map(url => Texture.from(url));
+    super(textures);
 
     this.x = 10;
     this.y = app.renderer.height / 2 - this.height / 2;
     this.scale.set(1.5, 1.5);
-    app.stage.addChild(this);
+    this.stage = app.stage;
+    this.animationSpeed = 0.167;
+    this.play();
+    this.stage.addChild(this);
   }
 
   public move(direction: string) {
@@ -32,27 +38,35 @@ export default class Ship extends Sprite {
     }
   }
 
+  public shoot(bullets: Array<any>) {
+    const bullet = new Sprite(Texture.from('assets/missile.png')); // TODO: create spritesheet + probably need a different missile img
+    bullet.position.x = this.x + this.width;
+    bullet.position.y = this.y;
+    this.stage.addChild(bullet);
+    bullets.push(bullet);
+  }
+
   private _moveUp() {
-    if (this.y > 5) {
-      this.y -= 5;
+    if (this.y > Ship.SPEED) {
+      this.y -= Ship.SPEED;
     }
   }
 
   private _moveDown() {
-    if (this.y < CANVAS_HEIGHT - this.height) {
-      this.y += 5;
+    if (this.y < CANVAS_HEIGHT - this.height - Ship.SPEED) {
+      this.y += Ship.SPEED;
     }
   }
 
   private _moveLeft() {
-    if (this.x > 5) {
-      this.x -= 5;
+    if (this.x > Ship.SPEED) {
+      this.x -= Ship.SPEED;
     }
   }
 
   private _moveRight() {
-    if (this.x < CANVAS_WIDTH - this.width) {
-      this.x += 5;
+    if (this.x < CANVAS_WIDTH - this.width - Ship.SPEED) {
+      this.x += Ship.SPEED;
     }
   }
 
