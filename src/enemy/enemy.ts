afc684265c2b19e58f223896c39efab3c6e336cf
FKinga92 <felfalusi.kinga@gmail.com>
2020-11-24 17:55:12 +0100

    move random int function to utils.ts

--- src/enemy/enemy.ts
+++ src/enemy/enemy.ts
@@ -1,5 +1,6 @@
 import { Application, Sprite, Texture } from 'pixi.js';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../constants';
+import { getRandomIntBetween } from '../utils';
 
 export default class Enemy extends Sprite {
 
@@ -12,9 +13,9 @@ export default class Enemy extends Sprite {
     super(texture);
     this.scale.set(1.5, 1.5);
     this.position.x = CANVAS_WIDTH - this.width;
-    this.position.y = this._randomInt(CANVAS_HEIGHT - this.height);
+    this.position.y = getRandomIntBetween(CANVAS_HEIGHT - this.height);
     app.stage.addChild(this);
-    this._direction = (this._randomInt(10) % 2 === 0) ? -1 : 1;
+    this._direction = (getRandomIntBetween(10) % 2 === 0) ? -1 : 1;
   }
 
   public updatePosition() {
@@ -22,7 +23,7 @@ export default class Enemy extends Sprite {
   }
 
   public changeDirection() {
-    if (this._randomInt(10) % 2 === 0) {
+    if (getRandomIntBetween(10) % 2 === 0) {
       this._changeDirection();
     }
   }
@@ -32,18 +33,14 @@ export default class Enemy extends Sprite {
   }
 
   private _move() {
-    this.position.y += (this._direction * Enemy.SPEED * this._randomInt(5));
+    this.position.y += (this._direction * Enemy.SPEED * getRandomIntBetween(5));
     if (this._hitsWall()) {
       this._changeDirection();
     }
-    this.position.x -= (0.1 * this._randomInt(5));
+    this.position.x -= (0.1 * getRandomIntBetween(5));
   }
 
   private _hitsWall(): boolean {
     return this.position.y <= 0 || this.position.y >= CANVAS_HEIGHT - this.height;
-  }
-
-  private _randomInt(max: number, min = 0): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
